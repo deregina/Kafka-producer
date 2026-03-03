0b38c51979848e123208ca7fb8ca04be68adde27
sejeong <sejeongmoon@gmail.com>
2026-03-03 14:09:04 +0900

    14

--- deploy/replace_secret.py
+++ deploy/replace_secret.py
@@ -1,97 +1,27 @@
-import requests
-import json
-from json.decoder import JSONDecodeError
-import logging
-from logging.handlers import TimedRotatingFileHandler
-import time
+import yaml
 import os
-import traceback
 
+root_dir = '/src/kafka-producer'
+with open(f'{root_dir}/config/application.yml','r') as config:
+    all_conf_dict = yaml.load(config, Loader=yaml.FullLoader)
 
-class RealtimeBicycle:
+for (root, dirs, files) in os.walk(root_dir):
+    # root는 문자열로, dirs와 files는 리스트로 리턴됨
+    if root.find('/.git') > 0 or root.find('/config/') > 0 or root.find('/.github/') > 0:
+        # 치환 불필요 디렉토리
+        continue
 
-    def __init__(self, dataset_nm):
-        self.auth_key = '##auth_key_seoul_data##'
-        self.api_url = 'http://openapi.seoul.go.kr:8088'
-        self.log_dir = '/log/seoul_api'
-        self.dataset_nm = dataset_nm
-        self.chk_dir()
-        self.log = self._get_logger()
+    # files 가 1개 이상 있을 경우 실행
+    if len(files) > 0:
+        for file in files:
+            # 치환 대상은 .py 파일만 대상으로 함
+            if file.endswith('.py'):
+                with open(f'{root}/{file}', 'r', encoding='utf-8') as file_read:
+                    py_file_all = ''.join(file_read.readlines())
+                    with open(f'{root}/{file}', 'w', encoding='utf-8') as file_write:
 
-    def call(self):
-        # url 형태: http://openapi.seoul.go.kr:8088/(인증키)/json/bikeList/1/5/
-        base_url = f'{self.api_url}/{self.auth_key}/json/{self.dataset_nm}'
-        start = 1
-        end = 1000
-        total_rows = []
-        while True:
-            try:
-                rslt = self._call_api(base_url, start, end)
-                contents = json.loads(rslt.text)
-            except JSONDecodeError:
-                self.log.error(f'요청 실패, {traceback.format_exc()}')
-                time.sleep(30)  # 30초 대기 후 재시도
-                continue
-
-
-            # 정상이 아닌 경우 처리
-            rslt_code = contents.get('CODE')
-            if rslt_code:
-                # INFO-200: 해당하는 데이터 없음. total_rows 리스트에 값이 존재할 경우
-                # 조회 범위 초과로 에러 발생한 것이며 결과 리턴하고 종료
-                if rslt_code == 'INFO-200' and total_rows:
-                    return total_rows
-                else:
-                    rslt_msg = contents.get('MESSAGE')
-                    self.log.error(f'요청 실패, 에러코드: {rslt_code}, 메시지:{rslt_msg}')
-                    time.sleep(30)      # 30초 대기
-
-            key_nm = list(contents.keys())[0]
-            items = contents.get(key_nm)
-            item_cnt = items.get('list_total_count')
-            item_row = items.get('row')
-            self.log.info(f'{base_url}/{start}/{end} 조회 성공, 건수: {len(item_row)}')
-            if item_row:
-                total_rows += item_row
-            if item_cnt < 1000:
-                break
-            else:
-                start = end + 1
-                end += 1000
-        return total_rows
-
-    def _call_api(self, base_url, start, end, base_dt=''):
-        headers = {'Content-Type': 'application/json',
-                   'charset': 'utf-8',
-                   'Accept': '*/*'
-                   }
-        if len(base_dt) > 0:
-            url = f'{base_url}/{start}/{end}/{base_dt}'
-        else:
-            url = f'{base_url}/{start}/{end}'
-        rslt = requests.get(url, headers)
-        return rslt
-
-    def chk_dir(self):
-        os.makedirs(self.log_dir, exist_ok=True)
-
-    def _get_logger(self):
-        default_format = '%(asctime)s [%(levelname)s]:%(message)s'
-        logging.basicConfig(
-            format=default_format,
-            level=logging.INFO,
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-        formatter = logging.Formatter(default_format)
-        handler = TimedRotatingFileHandler(os.path.join(self.log_dir, 'call_bicycle_api.log'), when="midnight", backupCount=7)
-        handler.suffix = "%Y-%m-%d"
-        handler.setFormatter(formatter)
-        logger = logging.getLogger(__name__)
-        logger.addHandler(handler)
-
-        return logger
-
-
-real_bicycle = RealtimeBicycle(dataset_nm='bikeList')
-items = real_bicycle.call()
-print(items[0:10])
+                        # application.yml 파일의 모든 키, 값 쌍을 읽어 replace
+                        for conf_item_dict in list(all_conf_dict.values()):
+                            for k, v in conf_item_dict.items():
+                                py_file_all = py_file_all.replace(f'##{k}##',v)
+                            file_write.write(py_file_all)
